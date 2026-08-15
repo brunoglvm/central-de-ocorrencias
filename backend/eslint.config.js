21b66f969de14ede6bb5134fe1cfd86841378a85
brunoglvm <bgalvaomiranda302@gmail.com>
2026-08-14 21:48:38 -0300

    ci: ignore generated files in checks

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
+    ignores: ['dist/**', 'prisma/src/generated/prisma/**'],
+  },
+  {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
     extends: ['js/recommended'],
